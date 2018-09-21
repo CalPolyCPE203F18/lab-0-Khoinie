432891138605b5ea338ab69b206ba25a0ca21a88
Khoinie <32655064+Khoinie@users.noreply.github.com>
2018-09-21 14:39:46 -0700

    Update Lab00.java

--- Lab00.java
+++ Lab00.java
@@ -2,7 +2,29 @@ public class Lab00
 {
    public static void main(String[] args)
    {
+	//Declaring and initializing some variables
+	int x = 5;
+	String y = "hello";
+	double z = 9.8;
 
+	//Printing the variables; check later!!
+	System.out.println("x:" + x + "y:" + y + "z:" + z);
+      
+   //A list (make an array in java)
+   int[] nums = [3,6,-1,2];
+   for (int val : nums)
+      System.out.println(val);
+   
+   System.out.println("");
    
    }
+   
+   public static char_count(String s, char c)
+   {
+      int count = 0;
+      for (char ch: s)
+         if c == s[count] {
+            count++; 
+         }
+      return count;
 }
