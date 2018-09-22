bff2b4f98d64512beb5f7af9f4bb482bd7d366d5
Khoinie <catherinewei18@yahoo.com>
2018-09-21 22:29:22 -0700

    Almost done

--- Lab00.java
+++ Lab00.java
@@ -2,39 +2,39 @@ public class Lab00
 {
    public static void main(String[] args)
    {
-	//Declaring and initializing some variables
-	int x = 5;
-	String y = "hello";
-	double z = 9.8;
+      //Declaring and initializing some variables
+      int x = 5;
+      String y = "hello";
+      double z = 9.8;
 
-	//Printing the variables; check later!!
-	System.out.println("x:" + x + "y:" + y + "z:" + z);
+      //Printing the variables
+      System.out.println("x: " + x + " y: " + y + " z: " + z);
       
-   //A list (make an array in java)
-   int[] nums = [3,6,-1,2];
-   for (int val : nums) {
-      System.out.println(val);
+      //A list (make an array in java)
+      int[] nums = {3,6,-1,2};
+      for (int val : nums) {
+         System.out.println(val);
+      }
+   
+      //Call a function
+      int numFound = char_count(y, 'l');
+      System.out.println("Found: " + numFound);
+   
+      //A counting for loop
+      for (int i = 1; i < 11; i++) {
+         System.out.println(i);
+   }//NEED TO FIND EQUIVALENT FOR <end = ''>
+      System.out.println("");
+   
    }
    
-   //Call a function
-   int numFound = char_count(y, '1');
-   System.out.println("Found:" + numFound);
-   
-   //A counting for loop
-   for (int i = 1; i < 11; i++) {
-	   System.out.println(i);
-   }
-	   
-   System.out.println("");
-   
-   }
-   
-   public static char_count(String s, char c)
+   public static int char_count(String s, char c)
    {
       int count = 0;
-      for (char ch: s)
-         if ch == c {
+      for (int i=0; i<s.length(); i++)
+         if (s.charAt(i) == c) {
             count++; 
          }
       return count;
+}
 }
